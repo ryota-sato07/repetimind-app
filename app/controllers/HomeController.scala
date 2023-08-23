f0eedd54e9fbdeb83ea5169e8b940336d3dcd1bf
ryota-sato07 <satour.0722@gmail.com>
2023-08-23 17:47:04 +0900

    update controller

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,15 +1,30 @@
 package controllers
 
 import javax.inject._
+import scala.concurrent.ExecutionContext
 
 import play.api.libs.json.Json
 import play.api.mvc._
+import slick.jdbc.JdbcProfile
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
+
+import models.repository.OutputRepository
 
 @Singleton
-class TestController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class TestController @Inject()(
+  cc: ControllerComponents,
+  outputRepository: OutputRepository,
+  protected val dbConfigProvider: DatabaseConfigProvider
+)(implicit ec: ExecutionContext) extends AbstractController(cc) {
 
-  def title = Action {
-    Ok(Json.obj("content" -> "World"))
+
+  def title = Action async { implicit request =>
+    for {
+      outputs     <- outputRepository.list()
+      Some(output) = outputs.headOption
+    } yield {
+      Ok(Json.obj("content" -> output.content))
+    }
   }
 
   def postTest = Action {
