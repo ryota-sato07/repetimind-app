cb1a83e9cdd141c80a4f57cada5a8903faf8cc4e
ryota-sato07 <satour.0722@gmail.com>
2023-08-31 10:00:39 +0900

    update user types

--- app/models/data/user/User.scala
+++ app/models/data/user/User.scala
@@ -9,7 +9,7 @@ import shapeless.tag.@@
  */
 import User._
 case class User(
-  id:        Option[Long],    // 管理ID
+  id:        Option[ID],      // 管理ID
   lineId:    Option[String],  // LINE 識別ID
   name:      Option[String],  // ユーザー名
   email:     String,          // メールアドレス
@@ -21,6 +21,17 @@ case class User(
 object User {
 
   // --[ New types ]------------------------------------------------------------
+  // ID
+  trait IdTag
+  type ID = Long @@ IdTag
+  def tagID(id: Long): ID = tag[IdTag][Long](id)
+
+  // LINE ID
+  trait LineIdTag
+  type LineID = String @@ LineIdTag
+  def tagLineID(lineId: String): LineID = tag[LineIdTag][String](lineId)
+
+  // パスワード
   trait PasswordTag
   type Password = String @@ PasswordTag
   def tagPassword(pass: String): Password = tag[PasswordTag][String](pass)
